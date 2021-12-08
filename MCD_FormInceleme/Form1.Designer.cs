2cbd663ead325489bfaa1cda7bd05ddc148a911c
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-08 16:19:44 +0300

    Add project files.

---
+++ MCD_FormInceleme/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace MCD_FormInceleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAc = new System.Windows.Forms.Button();
+            this.txtMesaj = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAc
+            // 
+            this.btnAc.Location = new System.Drawing.Point(423, 241);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(139, 88);
+            this.btnAc.TabIndex = 0;
+            this.btnAc.Text = "Merhaba Dünya";
+            this.btnAc.UseVisualStyleBackColor = true;
+            this.btnAc.Click += new System.EventHandler(this.btnAc_Click);
+            // 
+            // txtMesaj
+            // 
+            this.txtMesaj.Location = new System.Drawing.Point(408, 200);
+            this.txtMesaj.Name = "txtMesaj";
+            this.txtMesaj.Size = new System.Drawing.Size(170, 22);
+            this.txtMesaj.TabIndex = 1;
+            this.txtMesaj.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtMesaj_MouseClick);
+            this.txtMesaj.Enter += new System.EventHandler(this.txtMesaj_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 612);
+            this.Controls.Add(this.txtMesaj);
+            this.Controls.Add(this.btnAc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.TextBox txtMesaj;
+    }
+}
+
